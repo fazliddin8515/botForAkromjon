3a5f3ca7a5be9b3e4cb3bdc7ed60d55c4d5936b9
Fazliddin O'sarov <fazliddinusarov1999@gamil.com>
2025-01-17 16:09:48 +0500

    add tests for getEnv()

---
+++ src/util/env.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it, vi } from "vitest";
+import { getEnv } from "./env";
+import { GetEnvError } from "./error";
+
+vi.stubGlobal("process", {
+  env: {
+    BOT_TOKEN: "this_is_fake_bot_token"
+  }
+});
+
+describe("getEnv()", () => {
+  it("should return an env value if it exists in the environment", () => {
+    expect(getEnv("BOT_TOKEN")).toBe("this_is_fake_bot_token");
+  });
+
+  it("should throw GetEnvError if the env is not specified in the environment", () => {
+    const getEnvFn = () => getEnv("NOT_SET");
+    console.log(process.env["BOT_TOKEN"]);
+    expect(getEnvFn).toThrow(GetEnvError);
+  });
+});
